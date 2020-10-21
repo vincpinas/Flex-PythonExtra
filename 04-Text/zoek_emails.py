b1006fc2e4f963feedd6afcf15cef55c6b969138
vincent <ikbengerrit92@gmail.com>
2020-10-21 20:46:15 +0200

    simpele zoekmachine v1

---
+++ 04-Text/zoek_emails.py
@@ -0,0 +1,22 @@
+import re
+
+emails = []
+
+with open("tekstmetemails.txt", "r") as bestand:
+
+    regel = bestand.readline()
+   
+    while regel:
+        # Vul de juiste regular expression voor een email in op de puntjes
+        patroon = r"[\w\.-]+@[\w\.-]+"
+
+        # Gebruik de juiste code op de plaats van de puntjes
+        gevonden = re.findall(patroon, regel)
+
+        # Alle gevonden emails aan de email list toevoegen
+        emails.append(gevonden)
+        
+        # Volgende regel lezen
+        regel = bestand.readline()
+
+print(emails)
